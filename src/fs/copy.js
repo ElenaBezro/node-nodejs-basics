60951612a39eb9746429bd4f67e3922f8ef3ef08
elenaBezro <lenyka_86@mail.ru>
2022-12-05 15:19:25 +0100

    fit: copy a folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { getCurrentDirName } from "./utils.js";
+import { readdir, copyFile, mkdir } from "node:fs/promises";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const errMessage = "FS operation failed";
+  const sourceDirName = path.join(getCurrentDirName(), "files");
+  const destinationDirName = path.join(getCurrentDirName(), "files_copy");
+
+  try {
+    await mkdir(destinationDirName, { recursive: false });
+    const sourceDirContent = await readdir(sourceDirName);
+    await Promise.all(sourceDirContent.map((fileToCopy) => copyFile(`${sourceDirName}/${fileToCopy}`, `${destinationDirName}/${fileToCopy}`)));
+  } catch (e) {
+    throw new Error(errMessage);
+  }
 };
 
 copy();

f693f4acbd4dec6a69b6be4bdd981a1695f88956
elenaBezro <lenyka_86@mail.ru>
2022-12-06 00:24:56 +0100

    fit: cli arguments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const trimmedArguments = process.argv.slice(2);
+  const comLineArguments = trimmedArguments.reduce((acc, arg, index, array) => {
+    const trimArgName = (argument) => {
+      return argument.slice(2);
+    };
+
+    if (index % 2 === 0) {
+      acc.push(`${trimArgName(arg)} is ${array[index + 1]}`);
+    }
+    return acc;
+  }, []);
+
+  console.log(comLineArguments.join(", "));
 };
 
 parseArgs();

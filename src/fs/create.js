d31f5f1367fcd814a7d9d4fe74807e0962dce122
elenaBezro <lenyka_86@mail.ru>
2022-12-05 12:45:00 +0100

    fit: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access } from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const getDirName = () => {
+  const __filename = fileURLToPath(import.meta.url);
+  return dirname(__filename);
+};
+
 const create = async () => {
-    // Write your code here 
+  const errMessage = "FS operation failed";
+  const fileContents = "I am fresh and young";
+  const curDirPath = getDirName();
+  const filePath = path.join(curDirPath, "files", "fresh.txt");
+
+  try {
+    await writeFile(filePath, fileContents, { flag: "wx" });
+  } catch {
+    throw new Error(errMessage);
+  }
 };
 
 await create();

f6264d8d914c12f37082bda790edfbe9abe4bf1b
elenaBezro <lenyka_86@mail.ru>
2022-12-05 19:57:21 +0100

    fit: read a file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { getCurrentDirName } from "./utils.js";
+import { readFile } from "node:fs/promises";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const errMessage = "FS operation failed";
+  const fileToReadPath = path.join(getCurrentDirName(), "files", "fileToRead.txt");
+
+  try {
+    const fileContent = await readFile(fileToReadPath, "utf8");
+    console.log(fileContent);
+  } catch {
+    throw new Error(errMessage);
+  }
 };
 
 await read();
